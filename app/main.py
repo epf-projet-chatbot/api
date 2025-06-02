c4debc3b25415d6b08da826c0f078376cd4f056c
3lW30N <noemierialland2004@gmail.com>
2025-06-02 11:30:08 +0200

    fix: changed from pymongo to motor in order to support async

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
-from pymongo import MongoClient
+import motor.motor_asyncio
 
 app = FastAPI()
 
-client = MongoClient("mongodb://mongo:27017")
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://mongo:27017")
 db = client["test_db"]
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI with MongoDB!"}
+async def read_root():
+    return {"message": "Hello from FastAPI with MongoDB async!"}
 
 @app.get("/items")
-def get_items():
-    items = list(db.items.find({}, {"_id": 0}))
+async def get_items():
+    items_cursor = db.items.find({}, {"_id": 0})
+    items = await items_cursor.to_list(length=100)
     return {"items": items}
